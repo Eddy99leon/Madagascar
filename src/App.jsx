c205e83ad32a8443cbe1edfb6597e6c38c091c88
Eddy Léon <leoneddy504@gmail.com>
2023-11-26 18:32:35 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <>
+      <div className=" text-[100px] font-bloom text-Rouge">
+        Madagascar
+      </div>
+      <div className=" text-[100px] font-Montserrat font-semibold text-Vert">
+        Madagascar
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bloom: ["Bloom", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"]
+      },
+      colors: {
+        Noir: "#0b0014",
+        Rouge: "#f40000",
+        Vert: "#38b000",
+      }
+    },
+  },
+  plugins: [],
+}
+
