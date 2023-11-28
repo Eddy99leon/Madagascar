1598e29b34e927a46d477a58e551a459af15a8e5
Eddy Léon <leoneddy504@gmail.com>
2023-11-28 09:00:34 +0100

    add footer section

--- src/compenents/Footer.jsx
+++ src/compenents/Footer.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
+import madagascar from "../assets/madagascar.png"
+import { Link } from 'react-router-dom'
+import { PiFacebookLogoDuotone, PiGithubLogoDuotone, PiLinkedinLogoDuotone } from 'react-icons/pi'
 
 const Footer = () => {
   return (
-    <div className='h-[600px] bg-Noir'>
-      
-    </div>
+    <section className="bg-Noir">
+        <div className='max-w-5xl mx-auto px-3 pt-16 pb-12 flex justify-between border-t border-gray-900'>
+
+            <div>
+              <div className='flex items-center mb-3'>
+                <img src={madagascar} alt="logo" className='w-10 mr-1' />
+                <h1 className='text-2xl font-medium'>
+                  Mada
+                </h1>
+              </div>
+              <p className='max-w-[350px] text-gray-400 text-sm'>
+                Lorem ipsum dolor sit amet conse
+                Lorem ipsum dolor sit amet consectetur.
+                Lorem ipsum dolor sit amet.
+                Lorem ipsum dolor sit amet consectetur.
+                Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+
+            <div className='flex items-center gap-14'>
+              <div>
+                <h1 className='text-Rouge font-medium text-lg'>
+                  Quick link
+                </h1>
+                <ul className='flex flex-col text-gray-400'>
+                  <Link className='hover:text-gray-50'>Home</Link>
+                  <Link className='hover:text-gray-50'>Gallery</Link>
+                  <Link className='hover:text-gray-50'>Contact</Link>
+                </ul>
+              </div>
+              <div>
+                <h1 className='text-Rouge font-medium text-lg'>
+                  More in fact
+                </h1>
+                <ul className='flex flex-col text-gray-400'>
+                  <Link className='hover:text-gray-50'>Blog</Link>
+                  <Link className='hover:text-gray-50'>Cookies</Link>
+                  <Link className='hover:text-gray-50'>About</Link>
+                </ul>
+              </div>
+            </div>
+
+        </div>
+
+        <div className='border-t border-gray-900'>
+          <div className='max-w-5xl mx-auto px-3 py-3 flex justify-between items-center'>
+              <div className='font-medium'>
+                  © Coder with 🤍❤️💚 by<span className='font-semibold'> Eddy Andriamisaina.</span>
+              </div>
+              <div className='flex items-center space-x-3 text-3xl'>
+                <PiFacebookLogoDuotone className='cursor-pointer' />
+                <PiLinkedinLogoDuotone className='cursor-pointer text-Rouge' />
+                <PiGithubLogoDuotone className='cursor-pointer text-Vert' />
+              </div>
+          </div>
+        </div>
+    </section>
   )
 }
 
