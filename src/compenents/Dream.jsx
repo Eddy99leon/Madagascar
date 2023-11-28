5836a72b6990246a1d74d912c2a3db519f25275e
Eddy Léon <leoneddy504@gmail.com>
2023-11-28 08:59:07 +0100

    add dream section

---
+++ src/compenents/Dream.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Dream = () => {
+  return (
+    <section className="bg-[url('./assets/mada2.jpg')] bg-center bg-no-repeat bg-cover h-[300px]">
+        <div className='bg-black/50 h-full flex justify-center items-center'>
+            <div className='max-w-5xl mx-auto px-3 py-6'>
+                <h1 className='text-5xl'>
+                    make your dream come true
+                </h1>
+                <p className='text-2xl'>
+                    Visite in masterclass of true.
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Dream
