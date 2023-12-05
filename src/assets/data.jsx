a288978823481df9486e19831fa38e706ede7f32
Eddy Léon <leoneddy504@gmail.com>
2023-12-05 07:14:03 +0100

    Add image data

--- src/assets/data.jsx
+++ src/assets/data.jsx
@@ -1,8 +1,21 @@
+import baobab from './ImgGallery/baobab.jpg'
+import panther from './ImgGallery/Panther.jpg'
+import zafimaniry from './ImgGallery/zafimaniry.jpg'
+import aloalo from './ImgGallery/aloalo.jpg'
+import villageZafimaniry from './ImgGallery/villageZafimaniry.jpg'
+import likeAlien from './ImgGallery/likeAlien.jpg'
+import PeopleZafimaniry from './ImgGallery/PeopleZafimaniry.jpg'
+import paradise from './ImgGallery/Paradise.jpg'
+import crowner from './ImgGallery/crowner.jpg'
+import SaintMarie from './ImgGallery/SaintMarie.jpg'
+import babakoto from './ImgGallery/babakoto.jpg'
+import isalo from './ImgGallery/isalo1.jpg'
+
 const  Gallerys = [
     {
         "id": 1,
-        "name": "The baobab of Madagascar",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/406059467_932849448318087_3991190141101794227_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeHe38DAh3Ail9etceXRQXLcO-r2IIFNTdw76vYggU1N3J0W26OCzf12_Yes0DvRHicMJIFG-zzfBWeDTVXYc7rH&_nc_ohc=wqEfuK2B0JQAX9max4-&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfANnitkPr4LuADSDY1EWxyesaoJ8OIf0IS1yiTt4Vha6g&oe=656C9A4B",
+        "name": "The Baobab",
+        "picture": baobab,
         "description": "They are the landmarks of a very special island - the baobabs. Madagascar is bursting with endemism and is a magnet for many photographers.",
         "location": "Mahajanga"
     },
@@ -16,14 +29,14 @@ const  Gallerys = [
     {
         "id": 3,
         "name" : "The  ZAFIMANIRY",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/339752846_177757128454921_4278091353024544595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=c42490&_nc_eui2=AeGfs0ZagKT1Z_pMBSy_nD7lVM4rZ6lZbkVUzitnqVluRRms9bldeNa2IqWqUrO5kqn6FIgH0pwkT_4Jh-ph-6dF&_nc_ohc=OVGrpprqu_AAX-wy1g5&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfCYlKzar8tCZAg5xAtTgfwDgkAPKuUIFAIYhZqUd0ORbA&oe=656CFBFE",
+        "picture": zafimaniry,
         "description": "ZAFIMANIRY dia foko  iray ao anatin'ny faritanin'ny Fianarantsoa. Ao anaty faritra roa @ renivohi-paritanin'ny foko BETSILEO no misy azy.",
         "location": "Fianarantsoa"
     },
     {
         "id": 4,
         "name" : "Panther Chameleons",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/405986107_932282785041420_5883719017762169942_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeESLyTyyQBYgOLsbyI8EydqEug2wwGmO4US6DbDAaY7hTTDUvZ54znNJ6djUut15C52HvMKS6Ty2hdJ3_WddYpz&_nc_ohc=TmiXqS5JjDMAX-bsC6B&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfCaQy_4Us0iPt0QR3Hw0fdxsqhX5-q11j2MOffbLcPX2w&oe=656BA5EF",
+        "picture": panther,
         "description": "They are undoubtedly among the most colorful reptiles in the world. Along their distribution areas, these fascinating reptiles have different color variations.",
         "location": "Toliara"
     },
@@ -44,63 +57,63 @@ const  Gallerys = [
     {
         "id": 7,
         "name" : "Paradise Flycatcher",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/403900647_931564851779880_4259390847075072727_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeHy3PXT-6sYHksnpj32iQt2dRWq4MB6VzB1FargwHpXMBs66JbsS0jVflq7vGRaGeVQhlu0hnOx9bkVrODifaEo&_nc_ohc=pTNPTsiIPEIAX_iawBL&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfAOOPJvS48XjH5xjWXWbDXz8WM2YByF9MurOiYWvLaeOQ&oe=656CBD44",
+        "picture": paradise,
         "description": "Small but mighty! The Madagascar paradise flycatcher (Terpsiphone mutata) is certainly one of the most beautiful birds on the island.",
         "location": "Toliara"
     },
     {
         "id": 8,
         "name" : "The ALOALO",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/273671520_488760985962496_2655658432507222080_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeFlkK5nmgj6H9zEsOnerdnU4yqnctYS-anjKqdy1hL5qQtyxtaQ1q_P6xUXTrmRywjAUqk1druYjtLfHM1qzzLC&_nc_ohc=Xcu75Axe5BsAX_ZFTlz&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfD3JUmN4doOTyxPiTf_t4K9JGCu5ooDiW_2YoCZPjQHsw&oe=656CE0D1",
+        "picture": aloalo,
         "description": "Ny Aloalo dia mariky ny fahafatesana eo amin`ny  foko Mahafaly sy ny foko Tandroy.",
         "location": "Toliara"
     },
     {
         "id": 9,
         "name" : "Crowned Lemurs",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/403828619_931068831829482_6463822741728511540_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEoxER3GYMFdx2qxaNe0FOxOnK2FeNvWcY6crYV429ZxudVS4KYsKuo-GTso3tnqMtdG97-MquU4j2MVYD_9pCp&_nc_ohc=QYgToFKz47oAX_IgwvM&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfCIwUVQF0dAiFqLxFYwgbcFeEuGeBJL0LqPyHzQoRnMbg&oe=656B5D13",
+        "picture": crowner,
         "description": "A small family of crowned lemurs in the jungle in northwest Madagascar. Experience the wilderness of the island of Madagascar.",
         "location": "Toliara"
     },
     {
         "id": 10,
         "name" : "Beach of Sainte-Marie",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/405490933_18242618353213404_8232704838699481607_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEJ98ls9c3Ji2lknvJIszvw08HtvulbVlDTwe2-6VtWUNgyGhJuVwJYn31kmWjqf4Hq4e9aZnAjWecWjWFdK3Wr&_nc_ohc=5JUuJGcXwP0AX-RrQMB&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfC3jcy8vmyOsVtKqgGR1175jGiqaHHcNQm5Xi6i3Q1LQQ&oe=656BD58F",
+        "picture": SaintMarie,
         "description": "Enchanted Treasure Islands of Ile Sainte-Marie and Ile Aux Nattes in Sambatra Beach Lodge Madagascar.",
         "location": "Sainte-Marie"
     },
     {
         "id": 11,
         "name" : "The Babakoto",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/404490094_928117135457985_5381676974409969597_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeEu79lwj_0MWWPJw05CfrndrN0d9CVB2qGs3R30JUHaoVAJe7vKbbQsjnyXZRYszoQJdW7lAWWtlDQHCZbJ8NEg&_nc_ohc=2trSULbTKHkAX9qY3xf&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfC67ufMal10iNQuG9Z0MeAI3mFu_UePGuCtWQy9NrQIOg&oe=656BE931",
+        "picture": babakoto,
         "description": "There is always a lot to discover and photograph on our photo tours. A special highlight is always the encounter with Madagascar's largest lemur.",
         "location": "Sainte-Marie"
     },
     {
         "id": 12,
         "name" : "People  ZAFIMANIRY",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/340077926_1972525523100235_1851149285098411243_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=c42490&_nc_eui2=AeHklesJR2IPT64BafTNuTwgHIrSTA59PQscitJMDn09C_DjiFQq4kzVXTsn1-dkSfyfQuDE7o0wQVSa6-3m5ixB&_nc_ohc=-VHIs7Z_UWIAX9bIRsY&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfAW4qupHtC5QJXFz3fUosoMzDMYdM0QrXZtow_BuGFJOg&oe=656C74A3",
+        "picture": PeopleZafimaniry,
         "description": "ZAFIMANIRY dia foko  iray ao anatin'ny faritanin'ny Fianarantsoa. Ao anaty faritra roa @ renivohi-paritanin'ny foko BETSILEO no misy azy.",
         "location": "Fianarantsoa"
     },
     {
         "id": 13,
-        "name" : "Discover the remote",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/382981118_898497361753296_6221534712532161419_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeFIQxuisntf0IyBzlNOIs1vH70yBfS8xKYfvTIF9LzEpspeLsN7YKfckRRuS9JgtiwO8ifeticrMlKDJQmEvBj-&_nc_ohc=ccdnARqRZwgAX8Vcqox&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfClfSnXl4iPTDijdjBdvaB-caOM3QOgOzzwLrWZdjYAmQ&oe=656C9B26",
+        "name" : "Discover Isalo",
+        "picture": isalo,
         "description": "Visit the regions in the west of Madagascar and go to the evergreen rainforests of the island.",
         "location": "Sainte-Marie"
     },
     {
         "id": 14,
         "name" : "Village  ZAFIMANIRY",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/405561556_312410958268455_6136974915426922568_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeGJrk3ampCAOpx9nZRNyYLyOwYIXzHrIE47BghfMesgTi26W_weCsmaoBVb8HDXjS_3g4KzpCIx68C4g1Fd6tfm&_nc_ohc=kcZMQCm3dxwAX8n8o2N&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfCFF-NWu7WDptydkKzO3dPBgiQ_xKIuASlUwEo8QNJwBg&oe=656B6B70",
+        "picture": villageZafimaniry,
         "description": "ZAFIMANIRY dia foko  iray ao anatin'ny faritanin'ny Fianarantsoa. Ao anaty faritra roa @ renivohi-paritanin'ny foko BETSILEO no misy azy.",
         "location": "Fianarantsoa"
     },
     {
         "id": 15,
         "name" : "Like an Alien",
-        "picture": "https://scontent.ftmm2-1.fna.fbcdn.net/v/t39.30808-6/402202193_925626805707018_7209137418182771409_n.jpg?stp=dst-jpg_s720x720&_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_eui2=AeGR5iKNa0uwnENemYMBigYbkG2q0e0t0AyQbarR7S3QDJhTIQSFiqCeytPjSR4c35dNw0e2GWYp2_IOhpVpePpN&_nc_ohc=10xvT9uvR1wAX_6ulXv&_nc_ht=scontent.ftmm2-1.fna&oh=00_AfBCM3_p9YF915wPwhFvvfwwzKI_G3x509BmibIdR4aRrg&oe=656BB365",
+        "picture": likeAlien,
         "description": "They roam almost invisibly through the undergrowth of the rainforests, hunting anything they can catch.",
         "location": "Sainte-Marie"
     },
--- src/compenents/CardGallery.jsx
+++ src/compenents/CardGallery.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import { PiMapPinDuotone } from 'react-icons/pi'
 
 const CardGallery = ({ gallery }) => {
+    const { name, picture, description, location } = gallery
   return (
     <div className='max-w-[360px] border border-gray-800 shadow-sm shadow-gray-900 rounded-sm'>
         <div className='w-full h-[200px] overflow-hidden'>
-            {gallery && <img src={gallery.picture} alt="park" className=' object-cover h-full w-full' /> }
+            <img src={picture} alt="park" className=' object-cover h-full w-full' />
         </div>
         <div className='mx-3 pt-6 pb-3'>
             <div className='mb-4'>
                 <h1 className='font-medium text-Vert mb-2'>
-                    ✨ {gallery.name}
+                    ✨ {name}
                 </h1>
                 <p className='text-sm font-medium text-gray-500 text-justify min-h-[90px]'>
-                    {gallery.description}
+                    {description}
                 </p>
             </div>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center'>
                     <PiMapPinDuotone className="text-lg text-gray-400" />
                     <span className='font-medium text-gray-300 text-sm'>
-                        {gallery.location}
+                        {location}
                     </span>
                 </div>
                 <div>
